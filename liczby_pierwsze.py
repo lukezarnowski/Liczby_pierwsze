c13a1c7b40c3bd6dee68f9db4d9ec3920d72d646
Lukasz Zarnowski <lukasz.j.zarnowski@gmail.com>
2021-04-08 19:55:18 +0200

    Dodano algorytm na liczby pierwsze

---
+++ liczby_pierwsze.py
@@ -0,0 +1,23 @@
+# pper = 5
+
+
+# N=20
+
+
+def znajdz_pierwsze_n_liczb_pierwszych(n: int):
+    liczby_pierwsze = []
+    num = 2
+    while len(liczby_pierwsze) < n:
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                liczby_pierwsze.append(num)
+        num += 1
+    return liczby_pierwsze
+
+
+if __name__ == '__main__':
+    N = 20
+    print(znajdz_pierwsze_n_liczb_pierwszych(5))
